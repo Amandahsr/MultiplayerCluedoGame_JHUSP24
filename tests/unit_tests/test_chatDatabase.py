6ffab1b7772975ad408369162acca329a4172b45
Amanda SR Ho <amanda.hsr@u.nus.edu>
2024-03-17 03:25:14 -0700

    unit testing

--- tests/unit_tests/test_chatDatabase.py
+++ tests/unit_tests/test_chatDatabase.py
@@ -1,44 +1,96 @@
 import pytest
 import mongomock
+from pytz import timezone
+from datetime import datetime
 from clueless.chat_system.chatDatabase import chatDatabase
 
 
 # Create a mock database for testing
 @pytest.fixture
 def testdb():
+    # Create test database
+    testdb = chatDatabase()
     testClient = mongomock.MongoClient()
-    testdb = chatDatabase(client=testClient)
+    testdb.client = testClient
+    testdb.database = testClient["cluelessChatDatabase"]
+    testdb.chatMessages = testdb.database["chatMessages"]
+    testdb.game_active_status = True
 
     # Populate database with test messages
-    game_ID = 5267
-    testdb.store_chat_message(game_ID=game_ID, player_ID=1, player_Name="Tom", message="test message 1")
-    testdb.store_chat_message(game_ID=game_ID, player_ID=1, player_Name="Tom", message="test message 2")
-    testdb.store_chat_message(game_ID=game_ID, player_ID=2, player_Name="Jack", message="test message 3")
+    testdb.chatMessages.insert_one(
+        {
+            "player_ID": 1,
+            "player_Name": "Tom",
+            "message_ID": 1,
+            "message_time": datetime.now(timezone("US/Eastern")),
+            "message": "test message 1",
+        }
+    )
+    testdb.chatMessages.insert_one(
+        {
+            "player_ID": 1,
+            "player_Name": "Tom",
+            "message_ID": 2,
+            "message_time": datetime.now(timezone("US/Eastern")),
+            "message": "test message 2",
+        }
+    )
+    testdb.chatMessages.insert_one(
+        {
+            "player_ID": 2,
+            "player_Name": "Jack",
+            "message_ID": 3,
+            "message_time": datetime.now(timezone("US/Eastern")),
+            "message": "test message 3",
+        }
+    )
 
     return testdb
 
 
+def test_connect_chat_database():
+    # Initialize new database for testing
+    testdb = chatDatabase()
+    testClient = mongomock.MongoClient()
+
+    testdb.connect_chat_database(client=testClient)
+
+    assert testdb.client is testClient
+    assert testdb.game_active_status is True
+
+
+def test_disconnect_chat_database():
+    # Connect to new database for testing
+    testdb = chatDatabase()
+    testClient = mongomock.MongoClient()
+    testdb.client = testClient
+    testdb.database = testClient["cluelessChatDatabase"]
+    testdb.chatMessages = testdb.database["chatMessages"]
+    testdb.game_active_status = True
+
+    testdb.disconnect_chat_database()
+
+    assert testdb.game_active_status is False
+
+
 def test_get_all_chat_messages(testdb):
-    game_ID = 5267
-    result = list(testdb.get_all_chat_messages(game_ID))
+    result = list(testdb.get_all_chat_messages())
 
     assert len(result) == 3
 
 
 def test_store_chat_message(testdb):
     # Test parameters
-    game_ID = 5267
     player_ID = 1
     player_Name = "Tom"
     message = "test message"
 
-    testdb.store_chat_message(game_ID=game_ID, player_ID=player_ID, player_Name=player_Name, message=message)
+    testdb.store_chat_message(player_ID=player_ID, player_Name=player_Name, message=message)
 
     # Retrieve specific stored message for test check
-    result = testdb.chat_messages.find_one({"game_ID": game_ID, "message_ID": 4})
+    result = testdb.chatMessages.find_one({"message_ID": 4})
 
     assert list(result.keys()) == [
-        "game_ID",
         "player_ID",
         "player_Name",
         "message_ID",
@@ -51,10 +103,9 @@ def test_store_chat_message(testdb):
 
 def test_get_specific_message(testdb):
     # Test parameters
-    game_ID = 5267
     msg_ID = 3
 
-    result = testdb.get_specific_message(game_ID=game_ID, msg_ID=msg_ID)
+    result = testdb.get_specific_message(msg_ID=msg_ID)
     print(result)
 
     assert result["message"] == "test message 3"
@@ -62,9 +113,8 @@ def test_get_specific_message(testdb):
 
 def test_get_specific_message_invalid(testdb):
     # Test parameters
-    game_ID = 5267
     msg_ID = 10
 
-    result = testdb.get_specific_message(game_ID=game_ID, msg_ID=msg_ID)
+    result = testdb.get_specific_message(msg_ID=msg_ID)
 
-    assert result == "Message associated with game ID and msg ID is not found in database."
+    assert result == "Message associated with msg ID is not found in database."
