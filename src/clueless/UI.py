9c35fbae50a1c8e5eb6c691b740d80c9119dfed7
Hannah Holbert <78614315+hmholber@users.noreply.github.com>
2024-04-15 18:34:42 -0400

    Update UI.py
    adding buttons to the UI - super buggy

--- src/clueless/UI.py
+++ src/clueless/UI.py
@@ -82,11 +82,13 @@ class PlayerCard:
 
 
 class PlayerOptions:
-    def __init__(self, gameUI, options):
+    def __init__(self, gameUI, options, game_controls, screen):
         self.options = options
         self.gameUI = gameUI
         self.header_font = pygame.font.Font('freesansbold.ttf', 28)
         self.option_font = pygame.font.Font('freesansbold.ttf', 25)
+        self.screen = screen
+        self.game_controls = game_controls
 
     def display(self):
         print("Options:")
@@ -108,6 +110,104 @@ class PlayerOptions:
         surface.blit(header_text, header_rect)
         text_y += 100  # Increase y-coordinate to draw options text
 
+
+        move = Button(self.screen, "Move", surface.get_width() // 2, text_y )
+        text_y = text_y + 50
+        suggest = Button(self.screen, "Suggest", surface.get_width() // 2, text_y)
+        text_y = text_y + 50
+        accuse = Button(self.screen, "Accuse", surface.get_width() // 2, text_y)
+
+        running = True
+        while running:
+            move.draw_button()
+            suggest.draw_button()
+            accuse.draw_button()
+            
+            # took this block from main, needs editing. I just put it in 
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    print("Quit event detected")  # Debug print
+                    running = False
+                    pygame.quit()
+                    sys.exit()
+
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_x, mouse_y = pygame.mouse.get_pos()
+                    if move.check_button(mouse_x, mouse_y):
+                        
+                        running = False
+
+                        cards = self.game_controls.moves
+                        
+                     
+                    elif suggest.check_button(mouse_x, mouse_y):
+                        
+                        running = False
+
+                        cards = self.game_controls.cards
+                   
+                       
+                    elif accuse.check_button(mouse_x, mouse_y):
+                      
+                        running = False
+
+                        cards = self.game_controls.cards
+                       
+
+                    # call the function that clears the screen and plots the next set of options based on what the user selected. 
+                    # cards is the paramter that gets updated depending on the previous option. 
+                    option_buttons(self.screen, surface, cards)
+
+            pygame.display.update()
+
+        def option_buttons(self, screen, surface, cards):
+
+            # need to pass screen in. cards is meant to be the otions in game control relative to the choice made. May have to hardcode. 
+
+            self.next_move_buttons = []
+            for card in cards:
+                new_button = Button(screen, card, surface.get_width() // 2, text_y)
+                
+                # need to change where each new button gets drawn
+                text_y = text_y + 50
+                self.next_move_buttons.append(new_button)
+
+            running = True
+            while running:
+        
+                for button in self.next_move_buttons:
+                    button.draw_button()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    print("Quit event detected")  # Debug print
+                    running = False
+                    pygame.quit()
+                    sys.exit()
+
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_x, mouse_y = pygame.mouse.get_pos()
+                    for button in self.next_move_buttons:
+                        if button.check_button(mouse_x, mouse_y):
+                            #self.s.send(f"Select_move:{button.msg}".encode())
+                            print("move/suggest/accuse selected")  # Debug print
+
+                            # send selected move back to game controller for state changes
+                            selected_move = button.msg
+                            running = False
+
+
+            pygame.display.update()
+
+
+
+        # # Draw player options adjusted to subsurface
+        # for option in self.options:
+        #     text = self.option_font.render(option, True, text_color)
+        #     text_rect = text.get_rect()
+        #     text_rect.center = (surface.get_width() // 2, text_y)
+        #     surface.blit(text, text_rect)
+        #     text_y += 50  # Increase y-coordinate for the next option
         # Draw player options adjusted to subsurface
         for option in self.options:
             text = self.option_font.render(option, True, text_color)
