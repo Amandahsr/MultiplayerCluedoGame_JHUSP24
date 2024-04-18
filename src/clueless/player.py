f614715ace5639016a270c6c5d607fd7d7a856d9
Amanda SR Ho <amanda.hsr@u.nus.edu>
2024-04-18 01:54:19 -0700

    lint formatting

--- src/clueless/player.py
+++ src/clueless/player.py
@@ -1,9 +1,22 @@
 from os import environ
-environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
-#import pygame
+
+environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+# import pygame
+
 
 class Player:
-    def __init__(self, character, location, is_connected, id, start = True, moved = False, cards=None, in_room=False, in_corner_room=False):
+    def __init__(
+        self,
+        character,
+        location,
+        is_connected,
+        id,
+        start=True,
+        moved=False,
+        cards=None,
+        in_room=False,
+        in_corner_room=False,
+    ):
         self.id = id
         self.character = character
         self.location = location
@@ -16,32 +29,45 @@ class Player:
 
     def get_location(self):
         return self.location
+
     def get_character(self):
         return self.character
+
     def get_id(self):
         return self.id
+
     def get_cards(self):
         return self.cards
+
     def get_is_connected(self):
         return self.is_connected
+
     def get_in_room(self):
         return self.in_room
+
     def get_in_corner_room(self):
         return self.in_corner_room
-    
+
     def set_location(self, x):
         self.location = x
+
     def set_character(self, x):
         self.character = x
+
     def set_id(self, x):
         self.id = x
+
     def set_moved(self, x):
         self.moved = x
+
     def set_cards(self, x):
         self.cards = x
+
     def set_is_connected(self, x):
         self.is_connected = x
+
     def set_in_room(self, x):
         self.in_room = x
+
     def set_in_corner_room(self, x):
         self.in_corner_room = x
